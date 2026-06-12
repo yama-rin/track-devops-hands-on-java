15fbc86fde38ceed14093499c7dbb1495e670dac
yama-rin <rinka.yamakawa@tcs.com>
2026-06-12 13:59:11 +0900

    Update CalculatorTest.java

--- src/test/java/com/example/handson/CalculatorTest.java
+++ src/test/java/com/example/handson/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.example.handson;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
