7fc826177af826bad28af275a0f5b13ce7b002df
yama-rin <rinka.yamakawa@tcs.com>
2026-06-12 13:46:02 +0900

    Update Calculator.java
    2 to 3

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -1,7 +1,13 @@
 package com.example.handson;
 
 public class Calculator {
+  
   public double add(double a, double b) {
     return a + b;
   }
+
+  public double add(double a, double b, double c) {
+    return a + b + c;
+  }
+  
 }
