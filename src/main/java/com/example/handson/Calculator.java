eaba1693486a7df9fdacc857c7a39a3bef34cee6
yama-rin <rinka.yamakawa@tcs.com>
2026-06-12 13:54:02 +0900

    Update Calculator.java
    if文追加

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -7,7 +7,17 @@ public class Calculator {
   }
 
   public double add(double a, double b, double c) {
+    validateRange(a);
+    validateRange(b);
+    validateRange(c);
+    
     return a + b + c;
+  }
+
+  private void validateRange(double value) {
+    if (value < 0 || value > 10) {
+      throw new IllegalArgumentException("value must be between 0 and 10");
+    }
   }
   
 }
